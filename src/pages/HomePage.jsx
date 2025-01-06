969640206081c04be4ef20ebd604152cb0dbe0c9
Ahmed Hosny <ahmedHosny.R.M.E@gmail.com>
2025-01-06 15:04:51 +0200

    added hero section into home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,18 @@
+import pikachuImage from '../assets/pikachu.png';
+
 function HomePage() {
   return (
     <>
-      <section>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <section className="bg-gray-50 py-14 px-4">
+        <div className="max-w-screen-lg mx-auto">
+          <div className="flex items-center justify-center gap-4">
+            <img className="h-48" src={pikachuImage} alt="Pikachu" />
+            <h1 className="font-extrabold text-5xl -mt-14">
+              <span className="text-brand">Anime</span>
+              <br /> List
+            </h1>
+          </div>
+        </div>
       </section>
     </>
   );
