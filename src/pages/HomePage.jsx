04dce2214e9ea1ac63b8719c22e3648f07dcca27
Ahmed Hosny <ahmedHosny.R.M.E@gmail.com>
2025-01-06 11:54:42 +0200

    added tailwindcss to project dep

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ function HomePage() {
   return (
     <>
       <section>
-        <h1>Home Page</h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </section>
     </>
   );
