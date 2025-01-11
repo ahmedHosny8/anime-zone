13b9351efd08e57dc62bd98a561cda588b9a12c8
Ahmed Hosny <ahmedHosny.R.M.E@gmail.com>
2025-01-11 13:12:06 +0200

    remove json-server and from project dep

---
+++ src/components/SignupForm.jsx
@@ -0,0 +1,113 @@
+import { useForm } from 'react-hook-form';
+import Button from './Button';
+
+// Email regex: /\S+@\S+\.\S+/
+
+function SignupForm() {
+  const { register, handleSubmit, formState, getValues } = useForm();
+  const { errors } = formState;
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const onError = (errors) => {
+    console.log(errors);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit, onError)}
+      className="min-w-xs max-w-md mx-auto py-8 px-4 flex flex-col gap-4 bg-white border border-gray-200/80 rounded-2xl overflow-hidden shadow-2xl shadow-gray-500/10"
+    >
+      <div className="flex flex-col gap-1">
+        <label htmlFor="name" className="font-semibold">
+          Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          className="input input-bordered grow"
+          {...register('name', {
+            required: 'This field is required',
+            minLength: {
+              value: 2,
+              message: 'Must be at least 2 characters',
+            },
+          })}
+        />
+        {errors.name && (
+          <span className="text-red-500 text-sm">{errors.name.message}</span>
+        )}
+      </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="email" className="font-semibold">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="input input-bordered grow"
+          {...register('email', {
+            required: 'This field is required',
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: 'Please provide a valid email address',
+            },
+          })}
+        />
+        {errors.email && (
+          <span className="text-red-500 text-sm">{errors.email.message}</span>
+        )}
+      </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="password" className="font-semibold">
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="input input-bordered grow"
+          {...register('password', {
+            required: 'This field is required',
+            minLength: {
+              value: 8,
+              message: 'Password needs a minimum of 8 characters',
+            },
+          })}
+        />
+        {errors.password && (
+          <span className="text-red-500 text-sm">
+            {errors.password.message}
+          </span>
+        )}
+      </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="passwordConfirm" className="font-semibold">
+          Confirm password
+        </label>
+        <input
+          type="password"
+          id="passwordConfirm"
+          className="input input-bordered grow"
+          {...register('passwordConfirm', {
+            required: 'This field is required',
+            validate: (value) =>
+              value === getValues().password || 'Passwords need to match',
+          })}
+        />
+        {errors.passwordConfirm && (
+          <span className="text-red-500 text-sm">
+            {errors.passwordConfirm.message}
+          </span>
+        )}
+      </div>
+
+      <div className="mt-4 flex gap-4 justify-end">
+        <Button type="primary">Sign up</Button>
+      </div>
+    </form>
+  );
+}
+
+export default SignupForm;
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,3 +1,5 @@
+import SignupForm from '../components/SignupForm';
+
 function SignupPage() {
   return (
     <section className="h-screen bg-gray-50 py-14 px-4">
@@ -5,6 +7,7 @@ function SignupPage() {
         <h1 className="mb-10 font-extrabold text-4xl text-center">
           Create your account
         </h1>
+        <SignupForm />
       </div>
     </section>
   );
