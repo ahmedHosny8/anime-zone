cec685f03c414a57c6e3e3b4ce1bd1dbcc144b72
Ahmed Hosny <ahmedHosny.R.M.E@gmail.com>
2025-01-11 15:37:18 +0200

    setup json-server and json-server-auth correct

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,6 +1,31 @@
+import axios from 'axios';
 import SignupForm from '../components/SignupForm';
 
+const createUser = async (userData) => {
+  try {
+    const res = await axios.post('http://localhost:4000/signup', userData);
+    console.log(res.data);
+  } catch (error) {
+    console.error(
+      'Error creating user:',
+      error.response?.data || error.message
+    );
+  }
+};
+
 function SignupPage() {
+  const userData = {
+    email: 'Ali@mail.com',
+    password: 'bestPassw0rd',
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('now');
+
+    createUser(userData);
+  };
+
   return (
     <section className="h-screen bg-gray-50 py-14 px-4">
       <div className="max-w-screen-lg mx-auto">
@@ -8,6 +33,12 @@ function SignupPage() {
           Create your account
         </h1>
         <SignupForm />
+
+        <form onSubmit={handleSubmit}>
+          <input type="text" />
+
+          <button>Create</button>
+        </form>
       </div>
     </section>
   );
