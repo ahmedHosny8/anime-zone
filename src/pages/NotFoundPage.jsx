99dcddbefddd9963726698ca4d662575622e33ed
Ahmed Hosny <ahmedHosny.R.M.E@gmail.com>
2025-01-13 22:12:29 +0200

    done styling not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,8 +1,23 @@
+import { Frown } from 'lucide-react';
+import Button from '../components/Button';
+
 function NotFoundPage() {
   return (
     <>
-      <section>
-        <h1>Not Found Page</h1>
+      <section className="h-screen bg-gray-50 py-14 px-4">
+        <div className="max-w-screen-lg mx-auto">
+          <div className="flex flex-col items-center">
+            <Frown size={144} className="text-gray-400" />
+            <h1 className="font-extrabold text-5xl text-gray-500">404</h1>
+            <p className="mb-4 font-semibold text-lg text-gray-500">
+              Page no found
+            </p>
+
+            <Button to="/" variation="accent">
+              Go Home
+            </Button>
+          </div>
+        </div>
       </section>
     </>
   );
