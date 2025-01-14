1f375da4248d0268cd364212cf2a773aa429b3ce
Ahmed Hosny <ahmedHosny.R.M.E@gmail.com>
2025-01-14 13:18:42 +0200

    done fixing not found page layout

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -7,9 +7,11 @@ function NotFoundPage() {
       <section className="h-screen bg-gray-50 py-14 px-4">
         <div className="max-w-screen-lg mx-auto">
           <div className="flex flex-col items-center">
-            <Frown size={144} className="text-gray-400" />
-            <h1 className="font-extrabold text-5xl text-gray-500">404</h1>
-            <p className="mb-4 font-semibold text-lg text-gray-500">
+            <Frown size={144} strokeWidth={1} className="mb-2 text-gray-400" />
+            <h1 className="font-bold text-center text-4xl text-gray-500">
+              404
+            </h1>
+            <p className="mb-4 font-semibold text-3xl text-gray-500">
               Page no found
             </p>
 
