a96fb60a54c2985cb8f405f38dc3ba3b710d3ccb
Ahmed Hosny <ahmedHosny.R.M.E@gmail.com>
2025-01-18 14:17:31 +0200

    done fixing not found page colors

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -4,18 +4,19 @@ import Button from '../components/Button';
 function NotFoundPage() {
   return (
     <>
-      <section className="h-screen bg-gray-50 py-14 px-4">
+      <section className="min-h-screen bg-[var(--color-gray-50)] py-14 px-4">
         <div className="max-w-screen-lg mx-auto">
           <div className="flex flex-col items-center">
-            <Frown size={144} strokeWidth={1} className="mb-2 text-gray-400" />
-            <h1 className="font-bold text-center text-4xl text-gray-500">
-              404
-            </h1>
-            <p className="mb-4 font-semibold text-3xl text-gray-500">
-              Page no found
-            </p>
+            <Frown
+              size={144}
+              strokeWidth={1}
+              color="var(--color-gray-300)"
+              className="mb-2"
+            />
+            <h1 className="font-bold text-center text-4xl">404</h1>
+            <p className="mb-4 font-semibold text-3xl">Page no found</p>
 
-            <Button to="/" variation="accent">
+            <Button to="/" variation="secondary">
               Go Home
             </Button>
           </div>
